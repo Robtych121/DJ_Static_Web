a775f758ff31c7e9530a22d8f89cabfe65a359a2
Robert Davies <rob.davies1991@gmail.com>
2020-05-26 19:11:28 +0100

    removed login functions .. not required

--- dj_staticwebviewer/settings.py
+++ dj_staticwebviewer/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_forms_bootstrap',
-    'accounts',
+    'django_forms_bootstrap'
 ]
 
 MIDDLEWARE = [
